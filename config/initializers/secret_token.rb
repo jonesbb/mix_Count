0852039e8a34971177e837b4f3193f25e0e006e3
Byron Jones <jonesbb@gmail.com>
2012-08-24 14:01:28 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MixCount::Application.config.secret_token = 'aa06fea366ecf34535b13f3c1e3f462c75a9ace7c6590d2fc3afb914608b8744e84e62db6c7cb2c1d7f6027f562b8106fe6ba6a9065a03baa3357437c7774fdb'
